455244f465b37c23bce6c018e514af91dc16b5d6
Jess <jessicalaurenwilson1@gmail.com>
2023-10-25 16:33:35 +0100

    Add dprint formatter and format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from 'next'
-import { Poppins, Plus_Jakarta_Sans} from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Plus_Jakarta_Sans, Poppins } from 'next/font/google';
+import './globals.css';
 
 const poppins = Poppins({
-  weight: ['400', '600'],
-  style: ['normal'],
-  display: 'swap',
-  variable: '--font-poppins',
-  subsets: ['latin'],
-})
+	weight: ['400', '600'],
+	style: ['normal'],
+	display: 'swap',
+	variable: '--font-poppins',
+	subsets: ['latin'],
+});
 
 const jakarta = Plus_Jakarta_Sans({
-  weight: ['400', '500', '700'],
-  style: ['normal'],
-  display: 'swap',
-  variable: '--font-jakarta',
-  subsets: ['latin'],
-})
+	weight: ['400', '500', '700'],
+	style: ['normal'],
+	display: 'swap',
+	variable: '--font-jakarta',
+	subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-  title: 'Legacy Line',
-  description: 'Discover finanical freedom',
-}
+	title: 'Legacy Line',
+	description: 'Discover finanical freedom',
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={jakarta.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={jakarta.className}>{children}</body>
+		</html>
+	);
 }
