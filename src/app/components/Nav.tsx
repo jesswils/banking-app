64068d56f06a134a54e59997ee00918ac2966118
Jess <jessicalaurenwilson1@gmail.com>
2023-11-14 17:01:05 +0000

    finally fixed the navigation

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,16 +1,17 @@
 
 const Nav = () => {
   return (
-<nav>
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <span className="self-center text-xs font-sans font-semibold whitespace-nowrap text-white">LegacyLine.</span>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-transparent" aria-controls="navbar-default" aria-expanded="false">
+    <>
+<nav className="flex items-center justify-between flex-wrap p-6">
+    {/* <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-base text-transparent" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+    </button> */}
+    <span className="text-2xl font-sans font-semibold whitespace-nowrap text-white">LegacyLine.</span>
+
+    <div className="hidden w-full md:block md:w-auto pt-5" id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
         <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-transparent rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
@@ -30,8 +31,8 @@ const Nav = () => {
 </button>        </li>
       </ul>
     </div>
-  </div>
 </nav>
+</>
   )
 }
 
