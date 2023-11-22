6b971ee05c7554700bb106385d0996938ec94e5e
Jess <jessicalaurenwilson1@gmail.com>
2023-11-22 14:43:28 +0000

    Finish bank section

--- src/app/components/Banks.tsx
+++ src/app/components/Banks.tsx
@@ -1,18 +1,12 @@
-import Binance from '../../../public/svgs/binance.svg'
-import IBM from '../../../public/svgs/ibm.svg'
-import PNC from '../../../public/svgs/pnc.svg'
-import WorldBank from '../../../public/svgs/world-bank.svg'
-import Stripe from '../../../public/svgs/stripe.svg'
-
 
 const Banks = () => {
   return (<>
-<section className="bg-white/[0.20] w-screen h-[5rem] flex pt-[1.5rem] pr-[2rem] align-center gap-[0.625rem]">
-<img src={Binance} />
-<img src={IBM} />
-<img src={PNC} />
-<img src={WorldBank} />
-<img src={Stripe} />
+<section className=" flex justify-evenly justify-self-center pb-5 bg-white/[0.20] w-screen h-[5rem] flex pt-[1.5rem] pr-[2rem] align-center gap-[0.625rem]" aria-description='logos for banking providers'>
+<img src={'/svgs/binance.svg'} alt='Binance' />
+<img src={'/svgs/ibm.svg'} alt='IBM'/>
+<img src={'/svgs/pnc.svg'} alt='PNC' />
+<img src={'svgs/world-bank.svg'} alt='World Bank' />
+<img src={'/svgs/stripe.svg'} alt='Stripe' />
     </section>
     </>
   )
