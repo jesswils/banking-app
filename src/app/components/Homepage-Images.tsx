0f97523e791d53a8cb002e497e9ee16fa9264379
Jess <jessicalaurenwilson1@gmail.com>
2023-12-01 13:56:53 +0000

    Credit card almost done

--- src/app/components/Homepage-Images.tsx
+++ src/app/components/Homepage-Images.tsx
@@ -3,12 +3,26 @@ import React from 'react'
 const Images = () => {
   return (
     <>
-    <section className='absolute flex place-self-end justify-end mt-10 end-80 bottom-10'>
+    {/* mobile banking app */}
+    <section className='absolute flex place-self-end justify-end  end-80'>
           <img src={'/assets/body_internal.png'} alt='mobile Images' className='absolute w-[14.8935rem] h=[32.2115rem]' />
     <img src={'/assets/phone.png'} alt='mobile view of the banking app' className='w-[14.8935rem] h=[32.2115rem]' />
     </section>
-    <img src='/assets/gaussian_layer.png' alt='credit card' className='absolute right-96'/>
-    </>
+    <section>
+    <img src='/assets/gaussian_layer.png' alt='credit card' className='absolute right-24 top-[23rem]'/>
+    <img src='/assets/logo.svg' alt='credit card logo' className='absolute right-[19rem] top-[24rem]' />
+    
+    {/* credit card */}
+    <section className='absolute right-64 top-[27rem] text-xs uppercase'>
+    <p aria-description='credit card number' className='pt-3'>****  ****  ****  ****</p>
+    <p aria-description='credit card limit' className='font-semibold pt-[1.5rem]'>$1000 000 Limit</p>
+    <section className='absolute -right-24 -bottom-1'>
+    <p aria-description='credit card valid to'>Valid Till<br></br>  +
+    <img src='/assets/fi-sr-infinity.svg' alt='credit card valid to date' className='w-min inline pl-2' /></p>
+    </section>
+    </section>
+    </section>
+    </>     
   )
 }
 
