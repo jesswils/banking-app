643ba4a5a68f77a980c5377c10bfa59d808d946e
Jess <jessicalaurenwilson1@gmail.com>
2023-12-05 16:58:02 +0000

    make all assets svgs

--- src/app/components/Homepage-Images.tsx
+++ src/app/components/Homepage-Images.tsx
@@ -4,9 +4,10 @@ const Images = () => {
   return (
     <>
     {/* mobile phone */}
-    <section className='absolute flex place-self-end justify-end  end-80'>
-          <img src={'/assets/body_internal.png'} alt='mobile Images' className='absolute w-[14.8935rem] h=[32.2115rem]' />
-    <img src={'/assets/phone.png'} alt='mobile view of the banking app' className='w-[14.8935rem] h=[32.2115rem]' />
+    <section className='absolute flex place-self-end justify-end end-80'>
+          <img src={'/svgs/body_internal.svg'} alt='mobile phone' className='absolute w-[16.14038rem] h-[33.50463rem] -top-6' />
+    <img src={'/svgs/phone.svg'} alt='mobile view of the banking app' className='shadow w-[14.8935rem] h-[31rem]' />
+    <img src={'/svgs/notch_detail.svg'} alt='mobile view of the banking app' className='absolute left-24 top-2' />
     </section>
     <section>
     
@@ -15,7 +16,7 @@ const Images = () => {
       <img src='/svgs/pound.svg' alt='pound symbol' className='w-8 h-8 absolute right-16 top-[17rem]' />
     {/* credit card  */}
     <div id='credit-card' className=' bg-credit-card/60 border rounded-[0.9375rem] border-transparent w-[21.375rem] h-[11.875rem] absolute right-9 top-80 backdrop-blur-sm'/>
-    <img src='/assets/logo.svg' alt='credit card logo' className='absolute right-[18.7rem] top-[21rem]' />
+    <img src='/svgs/logo.svg' alt='credit card logo' className='absolute right-[18.7rem] top-[21rem]' />
     
     {/* credit card info */}
     <section className='absolute right-[12.5rem] top-96 text-xs uppercase'>
@@ -23,7 +24,7 @@ const Images = () => {
     <p aria-description='credit card limit' className='font-semibold pt-6 pr-4 text-base uppercase'>$1000 000 Limit</p>
     <section className='absolute -right-20 -bottom-1'>
     <p aria-description='credit card valid to'>Valid Till<br></br>  +
-    <img src='/assets/fi-sr-infinity.svg' alt='credit card valid to date' className='w-min inline pl-2' /></p>
+    <img src='/svgs/fi-sr-infinity.svg' alt='credit card valid to date' className='w-min inline pl-2' /></p>
     </section>
     </section>
     </section>
