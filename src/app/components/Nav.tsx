52b0317aaf40d9a3e1f0e89d6bc0b9a19b25d041
Jess <jessicalaurenwilson1@gmail.com>
2023-12-06 17:06:33 +0000

    remove unused button

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,15 +1,7 @@
 const Nav = () => {
 	return (
 		<>
-			<nav className='flex items-center justify-between flex-wrap p-6 ml-[16rem] mr-[16rem]'>
-				{
-					/* <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-base text-transparent" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button> */
-				}
+			<nav className='flex items-center justify-between flex-wrap p-6 ml-64 mr-64'>
 				<span className='text-2xl font-sans font-semibold whitespace-nowrap text-white'>LegacyLine.</span>
 
 				<div className='hidden w-full md:block md:w-auto pt-5' id='navbar-default'>
