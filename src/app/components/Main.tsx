74bb78c545c39f8cda2e3432d1b5dd8eaf161be9
Jess <jessicalaurenwilson1@gmail.com>
2023-12-06 14:54:47 +0000

    margins

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -6,10 +6,10 @@ const Main = () => {
 		<>
 			<section className='ml-[16rem] mr-[16rem]'>
 				<Phone />
-				<h1 className='text-[3.5rem] font-bold text-white tracking-wider leading-extra-loose w-[36.625rem] h-[13.125rem] mt-[5.25rem]'>
+				<h1 className='text-[3.5rem] font-bold text-white tracking-wider leading-extra-loose w-[36.625rem] h-[13.125rem] mt-16'>
 					Discover Financial Freedom with LegacyLine
 				</h1>
-				<p className='w-[36.625rem] text-transparent-purple mt-6'>
+				<p className='w-[36.625rem] text-transparent-purple mt-8 mb-2'>
 					Welcome to our bank's website, where we provide secure finanical solutions tailored to meet your unique needs.
 				</p>
 				{/* buttons */}
@@ -34,7 +34,7 @@ const Main = () => {
 				</ul>
 
 				{/* information */}
-				<ul className='flex pt-[1.5rem] pr-[2rem] align-center gap-[3rem] mb-[9rem]' aria-description='Bank information'>
+				<ul className='flex mt-5 pt-[1.5rem] pr-[2rem] align-center gap-[3rem] mb-[9rem]' aria-description='Bank information'>
 					<li className='text-white text-[2rem] font-semibold'>
 						{/* less than symbol */}
 						&#60;0.1%
